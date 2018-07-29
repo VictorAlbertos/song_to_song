fa799f6439062b18829430571803ebee2448eb70
Victor Albertos <victor-albertos@cookpad.com>
2018-07-29 10:55:00 +0200

    add loop to mimic a user interface

--- src/main/java/song_to_song/Main.java
+++ src/main/java/song_to_song/Main.java
@@ -4,14 +4,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Artist artist = createArtist();
-        System.out.println(artist.fullName());
+        System.out.println("Welcome to song to song");
 
-        Song song = createSong(artist);
-        System.out.println(song.outputData());
+        System.out.println("Instructions");
+        System.out.println("Write #1 to add an artist");
+        System.out.println("Write #2 to add a song");
+        System.out.println("Write #3 to exit program");
+
+        while (true) {
+            System.out.println("Enter your choice");
+
+            Scanner input = new Scanner(System.in);
+            int userChoice = input.nextInt();
+
+            if (userChoice == 1) {
+                Artist artist = createArtist();
+                System.out.println(artist.fullName());
+            } else if (userChoice == 2) {
+                Artist artist = createArtist();
+                Song song = createSong(artist);
+                System.out.println(song.outputData());
+            } else if (userChoice == 3) {
+                break;
+            }
+        }
     }
-
-
+    
     private static Artist createArtist() {
         Scanner input = new Scanner(System.in);
 
@@ -34,8 +52,8 @@ public class Main {
         String description = input.nextLine();
 
         System.out.println("Enter the song duration: ");
-        double duration = input.nextDouble();
 
+        double duration = input.nextDouble();
 
         return new Song(songName, description, duration, artist);
     }
