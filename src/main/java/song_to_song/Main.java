8fba857fe9834941573ebabf241632be61bc1e64
Aarón Jamet Orgilés <aaronjametorgiles@gmail.com>
2018-10-17 11:56:28 +0200

    set up basic control flow

--- src/main/java/song_to_song/Main.java
+++ src/main/java/song_to_song/Main.java
@@ -1,7 +1,40 @@
 package song_to_song;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        boolean closeProgram = false;
+        while (!closeProgram) {
+
+            int choice;
+            Scanner keyboard = new Scanner(System.in);
+
+            System.out.println("\nEnter your choice for adding data (1 - Artist, 2 - Song, 3 - Album) or 0 to close program: ");
+            choice = keyboard.nextInt();
+
+            switch (choice) {
+                case 0:
+                    closeProgram = true;
+                    break;
+                case 1:
+                    System.out.println("Enter the data of the artist: ");
+                    Artist.enterArtist();
+                    break;
+                case 2:
+                    System.out.println("Enter the data of the song: ");
+                    //enterSong();
+                    break;
+                case 3:
+                    System.out.println("Enter the data of the album: ");
+                    //enterAlbum();
+                    break;
+                default:
+                    System.out.println("\nYour choice doesn´t exist, try again.");
+                    break;
+            }
+        }
     }
 }
